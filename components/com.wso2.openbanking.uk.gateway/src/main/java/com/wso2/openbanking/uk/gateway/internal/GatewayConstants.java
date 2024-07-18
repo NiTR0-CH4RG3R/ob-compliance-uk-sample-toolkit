0947f126d4fe1f63da5caa7881c27db14c5498f2
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-07-18 16:00:04 +0530

    Added dev portal API endpoints to the GatewayDataHolder and added some constants to use as keys when accessing the URLs in the URL map in the data holder class.

---
+++ components/com.wso2.openbanking.uk.gateway/src/main/java/com/wso2/openbanking/uk/gateway/internal/GatewayConstants.java
@@ -0,0 +1,23 @@
+package com.wso2.openbanking.uk.gateway.internal;
+
+public class GatewayConstants {
+    // Keys for the URL map
+    public static final String URL_MAP_IS_HOST = "isHost";
+    public static final String URL_MAP_AM_HOST = "amHost";
+
+    public static final String URL_MAP_DEV_PORTAL_APP_GET = "devPortalAppGet";
+    public static final String URL_MAP_DEV_PORTAL_APP_CREATION = "devPortalAppCreation";
+    public static final String URL_MAP_DEV_PORTAL_APP_UPDATE = "devPortalAppUpdate";
+    public static final String URL_MAP_DEV_PORTAL_APP_DELETE = "devPortalAppDelete";
+    public static final String URL_MAP_DEV_PORTAL_APP_MAP_KEYS = "devPortalAppMapKeys";
+    public static final String URL_MAP_DEV_PORTAL_API_LIST = "devPortalApiList";
+    public static final String URL_MAP_DEV_PORTAL_API_LIST_SUBSCRIBE = "devPortalApiSubscribe";
+
+    public static final String RESOURCE_MAP_DEV_PORTAL_APP_GET = "resourceDevPortalAppGet";
+    public static final String RESOURCE_MAP_DEV_PORTAL_APP_CREATION = "resourceDevPortalAppCreation";
+    public static final String RESOURCE_MAP_DEV_PORTAL_APP_UPDATE = "resourceDevPortalAppUpdate";
+    public static final String RESOURCE_MAP_DEV_PORTAL_APP_DELETE = "resourceDevPortalAppDelete";
+    public static final String RESOURCE_MAP_DEV_PORTAL_APP_MAP_KEYS = "resourceDevPortalAppMapKeys";
+    public static final String RESOURCE_MAP_DEV_PORTAL_API_LIST = "resourceDevPortalApiList";
+    public static final String RESOURCE_MAP_DEV_PORTAL_API_LIST_SUBSCRIBE = "resourceDevPortalApiSubscribe";
+}
--- components/com.wso2.openbanking.uk.gateway/src/main/java/com/wso2/openbanking/uk/gateway/internal/GatewayDataHolder.java
+++ components/com.wso2.openbanking.uk.gateway/src/main/java/com/wso2/openbanking/uk/gateway/internal/GatewayDataHolder.java
@@ -3,6 +3,9 @@ package com.wso2.openbanking.uk.gateway.internal;
 import com.wso2.openbanking.uk.gateway.handler.core.OpenBankingAPIHandler;
 import com.wso2.openbanking.uk.gateway.handler.dcr.DCRHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class holds the data generated at runtime required for the gateway component.
  */
@@ -20,18 +23,110 @@ public class GatewayDataHolder {
         return instance;
     }
 
+    private final Map<String, String> devPortalResourcesMap = new HashMap<>();
+
     private OpenBankingAPIHandler handlerChain = null;
+    private final Map<String, String> urlMap = new HashMap<>();
 
     private GatewayDataHolder() {
         constructHandlerChain();
+        constructDevPortalResourceMap();
+        constructUrlMap();
     }
 
     public OpenBankingAPIHandler getHandlerChain() {
         return handlerChain;
     }
 
+    public String getUrl(String key) {
+        return urlMap.get(key);
+    }
+
     private void constructHandlerChain() {
         handlerChain = new OpenBankingAPIHandler();
         handlerChain.setNextHandler(new DCRHandler());
     }
+
+    private void constructDevPortalResourceMap() {
+        // TODO : Read from a configuration file.
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_GET,
+                "/api/am/devportal/v3/applications/{applicationId}"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_CREATION,
+                "/api/am/devportal/v3/applications"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_UPDATE,
+                "/api/am/devportal/v3/applications/{applicationId}"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_DELETE,
+                "/api/am/devportal/v3/applications/{applicationId}"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_MAP_KEYS,
+                "/api/am/devportal/v3/applications/{applicationId}/map-keys"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_API_LIST,
+                "api/am/devportal/v3/apis"
+        );
+
+        devPortalResourcesMap.put(
+                GatewayConstants.RESOURCE_MAP_DEV_PORTAL_API_LIST_SUBSCRIBE,
+                "/api/am/devportal/v3/subscriptions/multiple"
+        );
+    }
+
+    private void constructUrlMap() {
+        // TODO : Read from a configuration file.
+        urlMap.put(GatewayConstants.URL_MAP_IS_HOST, "https://localhost:9446");
+        urlMap.put(GatewayConstants.URL_MAP_AM_HOST, "https://localhost:9443");
+
+        // Construct the URL map for the dev portal resources.
+        String devPortalAppGetResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_GET);
+        String devPortalAppGetUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalAppGetResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_APP_GET, devPortalAppGetUrl);
+
+        String devPortalAppCreationResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_CREATION);
+        String devPortalAppCreationUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalAppCreationResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_APP_CREATION, devPortalAppCreationUrl);
+
+        String devPortalAppUpdateResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_UPDATE);
+        String devPortalAppUpdateUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalAppUpdateResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_APP_UPDATE, devPortalAppUpdateUrl);
+
+        String devPortalAppDeleteResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_DELETE);
+        String devPortalAppDeleteUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalAppDeleteResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_APP_DELETE, devPortalAppDeleteUrl);
+
+        String devPortalAppMapKeysResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_APP_MAP_KEYS);
+        String devPortalAppMapKeysUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalAppMapKeysResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_APP_MAP_KEYS, devPortalAppMapKeysUrl);
+
+        String devPortalApiListResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_API_LIST);
+        String devPortalApiListUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST) + devPortalApiListResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_API_LIST, devPortalApiListUrl);
+
+        String devPortalApiListSubscribeResource =
+                devPortalResourcesMap.get(GatewayConstants.RESOURCE_MAP_DEV_PORTAL_API_LIST_SUBSCRIBE);
+        String devPortalApiListSubscribeUrl = urlMap.get(GatewayConstants.URL_MAP_AM_HOST)
+                + devPortalApiListSubscribeResource;
+        urlMap.put(GatewayConstants.URL_MAP_DEV_PORTAL_API_LIST_SUBSCRIBE, devPortalApiListSubscribeUrl);
+    }
+
 }
