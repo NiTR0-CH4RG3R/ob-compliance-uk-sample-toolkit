2d2816251dcdbee7c975e71941220bec8a5ac925
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-07-23 10:09:18 +0530

    A small set of string utility functions.

---
+++ components/com.wso2.openbanking.uk.gateway/src/main/java/com/wso2/openbanking/uk/gateway/common/util/StringUtil.java
@@ -0,0 +1,12 @@
+package com.wso2.openbanking.uk.gateway.common.util;
+
+import java.text.Collator;
+import java.util.Locale;
+
+public class StringUtil {
+    public static boolean equalsIgnoreCase(String str1, String str2) {
+        Collator collator = Collator.getInstance(Locale.ENGLISH);
+        collator.setStrength(Collator.PRIMARY); // For case-insensitive comparison
+        return (collator.compare(str1, str2) == 0);
+    }
+}
