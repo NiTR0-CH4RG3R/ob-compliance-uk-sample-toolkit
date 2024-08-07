7a239f4fc96cc07d80986d940795df15b01adfd5
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-08-07 14:57:32 +0530

    Removed an unused function!

--- components/com.wso2.openbanking.uk.common/src/main/java/com/wso2/openbanking/uk/common/util/StringUtil.java
+++ components/com.wso2.openbanking.uk.common/src/main/java/com/wso2/openbanking/uk/common/util/StringUtil.java
@@ -4,12 +4,6 @@ import java.text.Collator;
 import java.util.Locale;
 
 public class StringUtil {
-    public static boolean equalsIgnoreCase(String str1, String str2) {
-        Collator collator = Collator.getInstance(Locale.ENGLISH);
-        collator.setStrength(Collator.PRIMARY); // For case-insensitive comparison
-        return (collator.compare(str1, str2) == 0);
-    }
-
     public static String sanitizeString(String str) {
         return str.trim().replaceAll("[\r\n]", "_");
     }
